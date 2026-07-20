23c547a0a310fa62bfde687eb7ba7ce1dcc59e93
Aatish044 <ms9891128119@gmail.com>
2026-07-21 01:56:42 +0530

    change to env

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = "your_secret_key"; // Replace with a secure key
+const SECRET_KEY = process.env.JWT_SECRET; // Replace with a secure key
 
 exports.authMiddleware = (req, res, next) => {
     const token = req.header("Authorization");
